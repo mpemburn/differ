3a41267c70015f6cad0a91eea44e6e76dbaf967f
Mark Pemburn <mpemburn@clarku.edu>
2024-08-13 15:08:46 -0400

    Add logic for New and Save functionality.

--- app/Livewire/SourceFiles.php
+++ app/Livewire/SourceFiles.php
@@ -11,13 +11,15 @@ use Livewire\Component;
 class SourceFiles extends Component
 {
     protected $listeners = [
-        'refresh-event' => '$refresh'
+        'refresh-event' => '$refresh',
+        'contents-changed' => 'hasChanged',
     ];
     public string $sourceName;
     public string $initialContents;
     public string $editor;
     public bool $loading = true;
-    public bool $canSave = false;
+    public bool $hasMessage = false;
+    public bool $canSave;
     public bool $showNewButton = true;
     public bool $showDeleteButton = false;
     protected SourceFilesService $service;
@@ -37,16 +39,33 @@ class SourceFiles extends Component
     public function newSource(): void
     {
         $this->clear();
+        $this->canSave = false;
+        $this->initialContents = '';
         $this->dispatch('focusTitle');
+    }
+
+    public function testName(): void
+    {
+        if (empty($this->sourceName)) {
+            return;
+        }
+
+        $this->hasMessage = Storage::exists(SourceFilesService::SOURCES_PATH . $this->sourceName);
+        $this->canSave = !$this->hasMessage && !empty($this->editor);
     }
 
     public function hasChanged(): void
     {
         $this->canSave = $this->initialContents !== $this->editor;
+        $this->dispatch('refresh-event');
     }
 
     public function save(): void
     {
+        if (empty($this->sourceName) || empty($this->editor)) {
+            return;
+        }
+
         $this->loading = true;
         Storage::put(SourceFilesService::SOURCES_PATH . $this->sourceName, $this->editor);
         sleep(1);
@@ -58,11 +77,11 @@ class SourceFiles extends Component
         $this->sourceName = '';
         $this->editor = '';
         $this->showDeleteButton = false;
+        $this->hasMessage = false;
     }
 
     public function confirmDelete(): void
     {
-        Log::debug($this->sourceName);
         $this->dispatch('openConfirmDeleteModal');
     }
 
@@ -73,6 +92,8 @@ class SourceFiles extends Component
 
     public function delete(): void
     {
+        Storage::delete(SourceFilesService::SOURCES_PATH . $this->sourceName);
+        $this->clear();
         $this->dispatch('closeConfirmDeleteModal');
     }
 
--- resources/views/livewire/source-files.blade.php
+++ resources/views/livewire/source-files.blade.php
@@ -6,8 +6,14 @@
                 <option value="{{ $source }}">{{ $source }}</option>
             @endforeach
         </select>
-        <input type="text" wire:model="sourceName" data-name="sourceName" placeholder="Source name">
-        <button class="btn btn-primary btn-sm" wire:click="save" {{ $canSave ? '' : 'disabled' }}>Save</button>
+        <input
+            type="text"
+            wire:model="sourceName"
+            wire:keydown="testName"
+            data-name="sourceName"
+            laceholder="Source name"
+        />
+        <button id="save_button" class="btn btn-primary btn-sm" wire:click="save" {{ $canSave ? '' : 'disabled' }}>Save</button>
         <button class="btn btn-primary btn-sm float-right" wire:click="clear">Clear</button>
         @if($showNewButton)
             <button class="btn btn-primary btn-sm" wire:click="newSource" wire:click="focusTitle">New</button>
@@ -24,8 +30,17 @@
                  height="24">
         </div>
     </div>
+    @if($hasMessage)
+        <div id="message" class="text-danger">"{{ $sourceName }}" exists. Please choose a different name.</div>
+    @endif
     <div id="editor">
-        <textarea id="source_editor" wire:model="editor" wire:keydown="hasChanged" placeholder="List of URLs"></textarea>
+        <textarea
+            id="source_editor"
+            wire:model="editor"
+            wire:keydown="hasChanged"
+            placeholder="Enter a list of URLs"
+        >
+        </textarea>
     </div>
     @include('dialogs.confirm-delete', ['type' => 'sources'])
 </div>
@@ -45,6 +60,9 @@
             $('[data-name="sourceName"]').focus();
         });
 
+        $('#source_editor').on('cut paste', function () {
+            Livewire.dispatch('contents-changed');
+        })
     });
 
 </script>
